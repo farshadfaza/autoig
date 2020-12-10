f5dae9b301ac455913544a33430d0124d730103c
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2020-12-10 03:48:40 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,53 +1,8 @@
-#pkg installer
-clear
-echo " "
-echo -e " \e[91m         ███████╗███████╗████████╗██╗   ██╗██████╗ \e[0m"
-echo -e " \e[91m         ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗\e[0m"
-echo -e " \e[91m         ███████╗█████╗     ██║   ██║   ██║██████╔╝\e[0m"
-echo -e " \e[91m         ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ \e[0m"
-echo -e " \e[91m         ███████║███████╗   ██║   ╚██████╔╝██║     \e[0m"
-echo -e " \e[91m         ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝  FOR AUTOIG TOOL.\e[0m\n" 
-echo " "                                        
-echo " "
-echo -e "                   \e[37mTRIBUTE TO\e[91m THE LINUX CHOICE\e[0m"
-echo " "
-echo -e "                        \e[37mSCRIPT BY\e[91m NITRO\e[0m"
-echo -e " "
-echo -e "             \e[91m SUBSCRIBE\e[92m (NOOB HACKERS)\e[91m ON YOUTUBE\e[0m"
-echo "  "
-echo -e "\e[91m      |-----------------------[\e[0mV 1.0\e[91m]-----------------------|"
-echo -e "\e[91m      |----------------------\e[0mUPDATE V1.0\e[91m--------------------|"
-echo -e "\e[91m      |-----------------------------------------------------|"
-echo -e "\e[91m      |                                                     |"
-echo -e "\e[91m      | \e[0mAutoig tool is a all in one instagram handling tool\e[91m |"
-echo -e "\e[91m      |\e[0mfrom this tool you can get lots of followers for free\e[91m|"
-echo -e "\e[91m      |                                                     |"
-echo -e "\e[91m      |\e[92mTIP:\e[0m To get followers quickly make yur id look legit\e[91m |"
-echo -e "\e[91m      |\e[0m and upload some posts this will increae followers.\e[91m  |"
-echo -e "\e[91m      |                                                     |"
-echo -e "\e[91m      | \e[0mSUBSCRIBE (\e[92mNOOB HACKERS\e[0m)\e[0m FOR MORE TOOLS AND VIDEOS\e[91m  |"
-echo -e "\e[91m      |                                                     |"
-echo -e "\e[91m      |-----------------------------------------------------|"
-echo -e "\e[91m      |-----------------------\e[0mFEATURES\e[91m----------------------|"
-echo -e "\e[91m      |-----------------------------------------------------|"
-echo " "
-sleep 10
-apt-get update > /dev/null 2>&1
-apt-get upgrade > /dev/null 2>&1
-termux-setup-storage
-pkg install git > /dev/null 2>&1
-pkg install zip > /dev/null 2>&1
-pkg install mpv > /dev/null 2>&1
-pkg install wget > /dev/null 2>&1
-pkg install openssh > /dev/null 2>&1
-pkg install python > /dev/null 2>&1
-pkg install python2 > /dev/null 2>&1
-pkg install openssl-tool > /dev/null 2>&1
-pkg install php > /dev/null 2>&1
-pip install lolcat > /dev/null 2>&1
-clear
-am start -a android.intent.action.VIEW -d https://bit.ly/nhytchannel > /dev/null 2>&1
-clear
-cd $HOME/autoig
-rm setup > /dev/null 2>&1
-bash autoig.sh
+echo '\e[92m                          ██████╗███████╗████████╗██╗   ██╗██████╗ 
+                                      ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
+                                      ███████╗█████╗     ██║   ██║   ██║██████╔╝
+                                      ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
+                                      ███████║███████╗   ██║   ╚██████╔╝██║     
+                                      ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝' 
+                                          
+
