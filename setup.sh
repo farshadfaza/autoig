6c54f035ef0dbca4a5d8ac064b677149d481a6d0
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2020-12-10 03:16:07 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,39 +14,3 @@ echo -e "                        \e[37mSCRIPT BY\e[91m NITRO\e[0m"
 echo -e " "
 echo -e "             \e[91m SUBSCRIBE\e[92m (NOOB HACKERS)\e[91m ON YOUTUBE\e[0m"
 echo "  "
-#packages and commands
-echo " "
-echo -e "\e[91m      |-----------------------[\e[0mV 1.0\e[91m]-----------------------|"
-echo -e "\e[91m      |----------------------\e[0mUPDATE V1.0\e[91m--------------------|"
-echo -e "\e[91m      |-----------------------------------------------------|"
-echo -e "\e[91m      |                                                     |"
-echo -e "\e[91m      | \e[0mAutoig tool is a all in one instagram handling tool\e[91m |"
-echo -e "\e[91m      |\e[0mfrom this tool you can get lots of followers for free\e[91m|"
-echo -e "\e[91m      |                                                     |"
-echo -e "\e[91m      |\e[92mTIP:\e[0m To get followers quickly make yur id look legit\e[91m |"
-echo -e "\e[91m      |\e[0m and upload some posts this will increae followers.\e[91m  |"
-echo -e "\e[91m      |                                                     |"
-echo -e "\e[91m      | \e[0mSUBSCRIBE (\e[92mNOOB HACKERS\e[0m)\e[0m FOR MORE TOOLS AND VIDEOS\e[91m  |"
-echo -e "\e[91m      |                                                     |"
-echo -e "\e[91m      |-----------------------------------------------------|"
-echo -e "\e[91m      |-----------------------\e[0mFEATURES\e[91m----------------------|"
-echo -e "\e[91m      |-----------------------------------------------------|"
-sleep 20
-echo " "
-dpkg --configure 
-apt-get update –fix-missing 
-apt-get upgrade –fix-missing 
-apt-get update 
-apt-get upgrade 
-pkg install python -y 
-pkg install openssh -y 
-pkg install openssl-tool -y 
-pkg install git -y 
-pkg install wget -y 
-pkg install curl -y 
-pkg install php -y 
-termux-setup-storage 
-am start -a android.intent.action.VIEW -d https://bit.ly/nhytchannel
-rm setup
-bash autoig.sh
-##################REMOVE############
